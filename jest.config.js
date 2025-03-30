b022014e3ff166f601a87d018c41ca8be730ffbd
Vinicius <71671043+viniciusyoshioka@users.noreply.github.com>
2025-03-30 01:53:36 -0300

    fix: Error testing transpiled files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,23 @@
+import { pathsToModuleNameMapper } from 'ts-jest'
+
+import tsconfig from './tsconfig.json' assert { type: 'json' }
+
+
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: false,
+  roots: ['<rootDir>'],
+  modulePaths: [tsconfig.compilerOptions.baseUrl],
+  moduleNameMapper: pathsToModuleNameMapper(tsconfig.compilerOptions.paths),
+  testMatch: [
+    '<rootDir>/src/**/*.test.ts',
+    '<rootDir>/src/**/*.spec.ts',
+  ],
+  testPathIgnorePatterns: [
+    'node_modules',
+    '<rootDir>/dist',
+    '<rootDir>/libs',
+  ],
 }
