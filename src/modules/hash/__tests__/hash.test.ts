b34900d7d90c766a48a9a2dfcc0a953ed66a2db6
Vinicius <71671043+viniciusyoshioka@users.noreply.github.com>
2025-03-30 18:09:07 -0300

    feat: Add hash module

---
+++ src/modules/hash/__tests__/hash.test.ts
@@ -0,0 +1,35 @@
+import process from 'node:process'
+
+import { Path } from '@utils'
+import { hash, HashType } from '../hash'
+
+
+describe('Test Hash module', () => {
+
+
+  const cwd = process.cwd()
+
+  const path = new Path('./src/modules/hash/__tests__/test_file.txt')
+  const expectedSha256 = '04d2f0aa6d6415cd1671b56695b9417fce416afb0e9106f08f752f86839e3ad3'
+
+  const mockPath = {
+    absolutePath: [cwd, './src/modules/hash/__tests__/not_existent_file.txt'].join(Path.separator),
+  } as Path
+
+
+  it('should use sha-256 algorithm by default', async () => {
+    const checksum = await hash(path)
+    expect(checksum).toBe(expectedSha256)
+  })
+
+  it('should throw if the path does not exists', () => {
+    expect(hash(mockPath))
+      .rejects
+      .toThrow(`ENOENT: no such file or directory, open '${mockPath.absolutePath}'`)
+  })
+
+  it('should return the correct hash when using SHA-256 hash type', async () => {
+    const checksum = await hash(path, HashType.SHA_256)
+    expect(checksum).toBe(expectedSha256)
+  })
+})
---
+++ src/modules/hash/hash.ts
@@ -0,0 +1,46 @@
+import { createHash } from 'node:crypto'
+import { createReadStream } from 'node:fs'
+
+import { Path } from '@utils'
+
+
+export enum HashType {
+  SHA_256 = 'sha256',
+}
+
+
+// TODO: Add validation to assert hashType is a valid enum value
+export async function hash(path: Path, hashType = HashType.SHA_256): Promise<string> {
+
+
+  const hashStream = createHash(hashType)
+  const fileStream = createReadStream(path.absolutePath)
+
+
+  return await new Promise<string>((resolve, reject) => {
+    hashStream.on('error', error => {
+      hashStream.destroy()
+      fileStream.destroy()
+      reject(error)
+    })
+
+
+    fileStream.on('data', chunk => {
+      hashStream.update(chunk)
+    })
+
+    fileStream.on('end', () => {
+      const digestedHash = hashStream.digest('hex')
+
+      hashStream.destroy()
+      fileStream.destroy()
+      resolve(digestedHash)
+    })
+
+    fileStream.on('error', error => {
+      hashStream.destroy()
+      fileStream.destroy()
+      reject(error)
+    })
+  })
+}
