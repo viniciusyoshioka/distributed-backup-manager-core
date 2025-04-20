de3607686cf6a11eb5b826215b44b1e9e217adf3
Vinicius <71671043+viniciusyoshioka@users.noreply.github.com>
2025-04-19 22:06:11 -0300

    refactor: Make `FileSystem` abstraction a unique class again as a variant is not needed

--- src/modules/file-system/file-system.ts
+++ src/modules/file-system/file-system.ts
@@ -1,47 +1,138 @@
+import fs, { CopySyncOptions, RmOptions } from 'node:fs'
+
 import { Path, PathType } from '../../utils'
 
 
-export abstract class FileSystem {
-  abstract readDirectory(path: Path): Promise<string[] | null>
+export class FileSystem {
+  readDirectory(path: Path): string[] | null {
+    if (!path.exists()) {
+      return []
+    }
+    if (path.type !== PathType.DIR) {
+      return null
+    }
+    return fs.readdirSync(path.absolutePath)
+  }
 
 
-  abstract createDirectory(path: Path): Promise<void>
+  createDirectory(path: Path): void {
+    if (path.exists()) {
+      throw new Error('Cannot create directory because something already exists in given path')
+    }
+
+    fs.mkdirSync(path.absolutePath, { recursive: true })
+  }
 
 
-  async delete(path: Path): Promise<void> {
+  delete(path: Path): void {
     if (path.type === PathType.FILE) {
-      await this.deleteFile(path)
+      this.deleteFile(path)
       return
     }
 
     if (path.type === PathType.DIR) {
-      await this.deleteDirectory(path)
+      this.deleteDirectory(path)
       return
     }
 
     throw new Error(`Path type ${path.type} is not supported by delete`)
   }
 
-  abstract deleteFile(path: Path): Promise<void>
+  deleteFile(path: Path): void {
+    const pathExists = path.exists()
+    if (!pathExists) {
+      throw new Error('Cannot delete file because it does not exists')
+    }
 
-  abstract deleteDirectory(path: Path): Promise<void>
+    if (path.type !== PathType.FILE) {
+      throw new Error('Cannot delete path because it is not a file')
+    }
+
+    const rmOptions: RmOptions = {
+      force: true,
+      recursive: true,
+    }
+
+    fs.rmSync(path.absolutePath, rmOptions)
+  }
+
+  deleteDirectory(path: Path): void {
+    const pathExists = path.exists()
+    if (!pathExists) {
+      throw new Error('Cannot delete directory because it does not exists')
+    }
+
+    if (path.type !== PathType.DIR) {
+      throw new Error('Cannot delete path because it is not a directory')
+    }
+
+    const rmOptions: RmOptions = {
+      force: true,
+      recursive: true,
+    }
+
+    fs.rmSync(path.absolutePath, rmOptions)
+  }
 
 
-  async copy(fromPath: Path, toPath: Path): Promise<void> {
+  copy(fromPath: Path, toPath: Path): void {
     if (fromPath.type === PathType.FILE) {
-      await this.copyFile(fromPath, toPath)
+      this.copyFile(fromPath, toPath)
       return
     }
 
     if (fromPath.type === PathType.DIR) {
-      await this.copyDirectory(fromPath, toPath)
+      this.copyDirectory(fromPath, toPath)
       return
     }
 
     throw new Error(`Path type ${fromPath.type} is not supported by copy`)
   }
 
-  abstract copyFile(fromPath: Path, toPath: Path): Promise<void>
+  copyFile(fromPath: Path, toPath: Path): void {
+    const fromPathExists = fromPath.exists()
+    if (!fromPathExists) {
+      throw new Error('Cannot copy file from fromPath to toPath because fromPath does not exists')
+    }
 
-  abstract copyDirectory(fromPath: Path, toPath: Path): Promise<void>
+    if (fromPath.type !== PathType.FILE) {
+      throw new Error('Cannot copy fromPath because it is not a file')
+    }
+
+    const toPathExists = toPath.exists()
+    if (toPathExists) {
+      this.delete(toPath)
+    }
+
+    const parentPath = new Path(toPath.parentAbsolutePath)
+    const parentPathExists = parentPath.exists()
+    if (!parentPathExists) {
+      this.createDirectory(parentPath)
+    }
+
+    fs.copyFileSync(fromPath.absolutePath, toPath.absolutePath)
+  }
+
+  copyDirectory(fromPath: Path, toPath: Path): void {
+    const fromPathExists = fromPath.exists()
+    if (!fromPathExists) {
+      throw new Error('Cannot copy directory from fromPath to toPath because fromPath does not exists')
+    }
+
+    if (fromPath.type !== PathType.DIR) {
+      throw new Error('Cannot copy fromPath because it is not a directory')
+    }
+
+    const toPathExists = toPath.exists()
+    if (toPathExists) {
+      this.delete(toPath)
+    }
+
+    const copySyncOptions: CopySyncOptions = {
+      force: true,
+      recursive: true,
+    }
+
+    fs.cpSync(fromPath.absolutePath, toPath.absolutePath, copySyncOptions)
+  }
 }
