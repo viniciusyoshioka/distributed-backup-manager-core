054fd3162672066a4f61ad2c659d9a9a5b5673c2
Vinicius <71671043+viniciusyoshioka@users.noreply.github.com>
2025-05-19 00:23:45 -0300

    feat: Create `UserSubClient` and add it to `SyncClient`

--- src/modules/sync-client/client.ts
+++ src/modules/sync-client/client.ts
@@ -1,14 +1,16 @@
 import { NetworkAddress } from '../network'
-import { PathSubClient } from './sub-clients'
+import { PathSubClient, UserSubClient } from './sub-clients'
 
 
 export class SyncClient {
 
 
   readonly path: PathSubClient
+  readonly user: UserSubClient
 
 
   constructor(serverAddress: NetworkAddress) {
     this.path = new PathSubClient(serverAddress)
+    this.user = new UserSubClient(serverAddress)
   }
 }
